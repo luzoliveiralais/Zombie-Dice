30f075dfae8a5b6e393f40d845c04031327d52a4
Laís de Oliveira Luz <luzoliveiralais@gmail.com>
2024-09-30 22:22:02 -0300

    alterando o código para execução da atividade

--- test/test.py
+++ test/test.py
@@ -2,14 +2,19 @@ import pytest
 from unittest.mock import patch
 from src.zombie_dice import iniciar_jogo, obter_quantidade_jogadores
 
-# Simula as entradas para dois jogadores, e para cada jogador, a resposta "n" para não continuar o turno
-@patch('builtins.input', side_effect=['Jogador1', 'Jogador2', 'n', 'n'])
+# Simula as entradas para dois jogadores e várias rodadas
+@patch('builtins.input', side_effect=[
+    'Jogador1', 'Jogador2',  # Nomes dos jogadores
+    'n',  # Jogador 1 decide parar
+    'n',  # Jogador 2 decide parar
+    'n',  # Jogador 1 decide parar
+    'n'   # Jogador 2 decide parar
+])
 def test_iniciar_jogo(mock_input):
     jogadores = 2
     placar_final = iniciar_jogo(jogadores)
     assert len(placar_final) == 2  # Verifica se o placar final foi criado corretamente
     assert placar_final[0]['cerebros'] == 0  # Verifica o placar inicial
-
 
 # Corrigindo o nome da função para obter_quantidade_jogadores
 @patch('builtins.input', side_effect=['1', '2'])  # Simulando entrada inválida e válida
