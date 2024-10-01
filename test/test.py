b282ae2b9cc7c7feccede520749f69e943aca8dd
Laís de Oliveira Luz <luzoliveiralais@gmail.com>
2024-09-30 22:37:53 -0300

    inserindo testes

--- test/test.py
+++ test/test.py
@@ -1,15 +1,15 @@
-import pytest
 from unittest.mock import patch
 from src.zombie_dice import iniciar_jogo, obter_quantidade_jogadores
 
-# Teste para verificar o início do jogo e registro dos jogadores
-@patch('builtins.input', side_effect=['2', 'Jogador1', 'Jogador2'])
+@patch('builtins.input', side_effect=[2, 'Jogador1', 'Jogador2'])
 def test_iniciar_jogo(mock_input):
-    # Simula a entrada de 2 jogadores
-    quantidade_jogadores = obter_quantidade_jogadores()
-    assert quantidade_jogadores == 2
+    # Testa a função iniciar_jogo com dois jogadores simulados
+    nomes = iniciar_jogo(2)
+    assert len(nomes) == 2
+    assert nomes == ['Jogador1', 'Jogador2']
 
-    # Simula o início do jogo com 2 jogadores
-    nomes_jogadores = iniciar_jogo(quantidade_jogadores)
-    assert len(nomes_jogadores) == 2
-    assert nomes_jogadores == ['Jogador1', 'Jogador2']
+@patch('builtins.input', side_effect=[2])
+def test_obter_quantidade_jogadores(mock_input):
+    # Testa a função obter_quantidade_jogadores simulando a entrada de "2"
+    jogadores = obter_quantidade_jogadores()
+    assert jogadores == 2
